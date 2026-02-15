30d36671ead68417c5c0c705bfbc673f7b94cc63
onkar dudhale <dudhaleonkar@gmail.com>
2026-02-15 13:09:00 +0530

    commit on main

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,8 +7,8 @@ const BestSeller = () => {
     return (
         <div className='mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 md:gap-20 lg:gap-30 gap-10 mt-6'>
-                {products.filter((product) => product.inStock).slice(0, 5).map((product, index) =>(
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  md:gap-3 lg:gap-5 gap-4 mt-6 '>
+                {products.filter((product) => product.inStock).slice(0, 5).map((product, index) => (
                     <ProductCard key={index} product={product} />
                 ))}
             </div>
--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -25,9 +25,9 @@ const AllProducts = () => {
                 <p className='text-2xl font-medium uppercase'>All Products</p>
                 <div className='w-16 h-0.5 bg-primary rounded-full'></div>
             </div>
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 md-gap-6 gap-3 mt-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 md-gap-8 gap-5 mt-6'>
                 {filteredProducts
-                    .filter((product) => product.inStock) // Filter in-stock products
+                    .filter((product) => product.inStock) 
                     .map((product, index) => (
                         <ProductCard key={index} product={product} />
                     ))}
