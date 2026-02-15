c3d6a6b00bb36bebe8744ad8fb61bddb135c645d
onkar dudhale <dudhaleonkar@gmail.com>
2026-02-15 13:45:09 +0530

    allowed origins added

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB()
 await connectCloudinary()
 
 //allowed multiple origins for CORS
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://greencart-server-orcin.vercel.app/'];
 
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 
